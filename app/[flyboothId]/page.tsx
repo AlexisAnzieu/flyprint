e176482621e84c61069864a65d569dc6029feaae
alexis <alexis.anzieu@gmail.com>
2024-04-25 20:50:44 -0400

    modify frontend

--- app/[flyboothId]/page.tsx
+++ app/[flyboothId]/page.tsx
@@ -6,6 +6,7 @@ import { CLOUDINARY_FOLDER, cloudName, uploadPreset } from "@/lib/constants";
 export default function Home({ params: { flyboothId } }: any) {
   const fileInputRef = useRef<HTMLInputElement>(null);
   const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   function createFormData(uploadPreset: string, file: File) {
     const fd = new FormData();
@@ -16,12 +17,14 @@ export default function Home({ params: { flyboothId } }: any) {
   }
 
   async function uploadFile(e: React.ChangeEvent<HTMLInputElement>) {
+    setIsLoading(true);
     const file = e.target.files?.[0];
     if (!file) return;
-    const url = `https://api.cloudinary.com/v1_1/${cloudName}/upload`;
-    const fd = createFormData(uploadPreset, e.target.files![0]);
 
     try {
+      const url = `https://api.cloudinary.com/v1_1/${cloudName}/upload`;
+      const fd = createFormData(uploadPreset, e.target.files![0]);
+
       const res = await fetch(url, {
         method: "POST",
         body: fd,
@@ -36,6 +39,8 @@ export default function Home({ params: { flyboothId } }: any) {
       }
     } catch (error) {
       setError("Erreur survenue lors de l'envoi de la photo");
+    } finally {
+      setIsLoading(false);
     }
   }
 
@@ -52,13 +57,18 @@ export default function Home({ params: { flyboothId } }: any) {
           ref={fileInputRef}
         />
         <button
-          onClick={() => fileInputRef.current?.click()}
-          className="px-4 sm:px-10 py-3 sm:py-5 text-3xl sm:text-5xl bg-white text-black rounded-xl mb-10"
+          onClick={() => !isLoading && fileInputRef.current?.click()}
+          className={`px-4 sm:px-10 py-3 sm:py-5 text-3xl sm:text-5xl rounded-xl mb-10 ${
+            isLoading ? "bg-gray-500" : "bg-white"
+          } text-black`}
+          disabled={isLoading}
         >
-          Clique ici pour <br /> prendre une photo <br /> 📷
+          {isLoading
+            ? "En impression..."
+            : "Clique ici pour prendre une photo 📷"}
         </button>
-        {error && <div className="text-red-500">{error}</div>}
-        <div className="text-sm sm:text-2xl">
+        {error && <div className="text-red-500 text-center pb-20">{error}</div>}
+        <div className=" text-xl sm:text-2xl text-center">
           (assure toi que la photo prise soit bien lumineuse)
         </div>
       </div>
