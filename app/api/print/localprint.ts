70989341ee481707762b43a7b05043d669f1e982
alexis <alexis.anzieu@gmail.com>
2024-05-31 11:10:17 -0400

    push local way

---
+++ app/api/print/localprint.ts
@@ -0,0 +1,83 @@
+import net from "net";
+import EscPosEncoder from "esc-pos-encoder";
+import sharp from "sharp";
+import { Image } from "canvas";
+
+const PICTURE_WIDTH = 528;
+const PICTURE_HEIGHT = 712;
+const LOGO_WIDTH = 200;
+const LOGO_HEIGHT = 200;
+const PRINTER_IP = "192.168.0.87";
+const PRINTER_PORT = 9100;
+
+const getImage = async ({ pictureUrl, width, height, rotate = 0 }: any) => {
+  const response = await fetch(pictureUrl);
+  const imageBuffer = await response.arrayBuffer();
+
+  const processedImageBuffer = await sharp(imageBuffer)
+    .rotate(rotate)
+    .resize(width, height)
+    .toBuffer();
+
+  const img = new Image();
+  img.src = processedImageBuffer;
+
+  return img;
+};
+
+export const printImage = async (pictureUrl: string): Promise<any> => {
+  const client = new net.Socket();
+  console.log("Created socket");
+
+  return new Promise((resolve, reject) => {
+    client.connect(PRINTER_PORT, PRINTER_IP, async function () {
+      console.log("Connected to the printer");
+
+      let encoder = new EscPosEncoder();
+
+      const logo = await getImage({
+        pictureUrl:
+          "https://res.cloudinary.com/dkbuiehgq/image/upload/v1713989859/session_1_zqo8jz.png",
+        width: LOGO_WIDTH,
+        height: LOGO_HEIGHT,
+      });
+      const image = await getImage({
+        pictureUrl,
+        width: PICTURE_WIDTH,
+        height: PICTURE_HEIGHT,
+        rotate: 90,
+      });
+      const date = new Date();
+      const dateString = date.toLocaleString("en-GB", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+      });
+
+      let result = encoder
+        .initialize()
+        .align("center")
+        .image(logo, LOGO_WIDTH, LOGO_HEIGHT, "atkinson")
+        .newline()
+        .image(image, PICTURE_WIDTH, PICTURE_HEIGHT, "atkinson")
+        .newline()
+        .line(dateString)
+        .line("Le Plateau - Montreal")
+        .newline()
+        .newline()
+        .newline()
+        .newline()
+        .cut()
+        .encode();
+
+      return client.write(result, () => {
+        client.end(() => {
+          console.log("Connection closed");
+          resolve("done");
+        });
+      });
+    });
+  });
+};
--- app/api/print/route.ts
+++ app/api/print/route.ts
@@ -1,26 +1,7 @@
 import { unstable_noStore as noStore } from "next/cache";
-import net from "net";
-import EscPosEncoder from "esc-pos-encoder";
-import sharp from "sharp";
-import { createCanvas, Image } from "canvas";
+import { printImage } from "./localprint";
 
-const getImage = async (url: string) => {
-  let response = await fetch(url);
-  let imageBuffer = await response.arrayBuffer();
-
-  let processedImageBuffer = await sharp(imageBuffer)
-    .resize(520, 800)
-    .rotate(90)
-    .toBuffer();
-
-  let img = new Image();
-  img.src = processedImageBuffer;
-
-  let canvas = createCanvas(img.width, img.height);
-  let ctx = canvas.getContext("2d");
-  ctx.drawImage(img, 0, 0, img.width, img.height);
-  return canvas;
-};
+const IS_LOCAL = process.env.NODE_ENV === "development";
 
 export async function GET(req: Request) {
   noStore();
@@ -35,10 +16,17 @@ export async function GET(req: Request) {
       return Response.json({ error: "pictureUrl is required" });
     }
 
+    if (IS_LOCAL) {
+      const res = await printImage(pictureUrl);
+      return Response.json(res);
+    }
+
     const res = await fetch(
       `http://70.81.36.26:9100/print?pictureUrl=${pictureUrl}`
     );
+
     const { error } = await res.json();
+
     if (error) {
       return Response.json({
         error,
@@ -47,47 +35,7 @@ export async function GET(req: Request) {
 
     return Response.json(res);
   } catch (error) {
+    console.log(error);
     return Response.json({ error: "Erreur sur l'API" });
   }
-
-  // const client = new net.Socket();
-
-  // console.log("Created socket");
-
-  // await new Promise((resolve, reject) => {
-  //   client.connect(9100, "192.168.0.87", async function () {
-  //     console.log("Connected to the printer");
-  //     let encoder = new EscPosEncoder();
-  //     const image = await getImage(pictureUrl);
-
-  //     let result = encoder
-  //       .initialize()
-  //       .image(image, 520, 800, "atkinson")
-  //       .newline()
-  //       .cut()
-  //       .encode();
-
-  //     // Write data to the printer
-  //     const isBufferFull = !client.write(result);
-
-  //     if (isBufferFull) {
-  //       console.log("Buffer full, waiting for drain event to write more data");
-  //       client.once("drain", doEnd);
-  //     } else {
-  //       doEnd();
-  //     }
-
-  //     function doEnd() {
-  //       client.end(() => {
-  //         // Close the connection
-  //         console.log("Connection closed");
-  //         resolve("done");
-  //       });
-  //     }
-  //   });
-  // });
-
-  // console.log("Data sent and connection closed");
-
-  // return Response.json("Printed");
 }
